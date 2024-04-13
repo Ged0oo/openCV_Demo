352759a4820bed6a40efe741bf40b8eb90352c83
Mohamed Nagy <mohmmednagy@gmail.com>
2024-04-13 15:18:04 +0200

    get gret streem

--- 01_ImportExportVideo/test.py
+++ 01_ImportExportVideo/test.py
@@ -4,7 +4,8 @@ cap = cv2.VideoCapture(0)
 
 while(1) :
     ret, frame = cap.read()
-    cv2.imshow("frame", frame)
+    grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow("frame", grey)
     k = (cv2.waitKey(1) & 0xFF) == ord('q')
     if k :
         break
