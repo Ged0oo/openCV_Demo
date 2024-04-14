afab536dcb58d024ec72f0815416afe1528d4b6d
Mohamed Nagy <mohmmednagy@gmail.com>
2024-04-14 22:14:07 +0200

    draw lines between two sucsesive left mouse clicks

--- 02_MouseEvents/test.py
+++ 02_MouseEvents/test.py
@@ -5,20 +5,19 @@ def ClickEvent(event, x, y, flages, param):
     font = cv2.FONT_HERSHEY_SIMPLEX
 
     if(event == cv2.EVENT_LBUTTONDOWN):
-        txt = 'X: ' + str(x) + ' Y: ' + str(y)
-        cv2.putText(img, txt, (x, y), font, 0.5, (0, 255, 255), 2, cv2.LINE_AA)
+        cv2.circle(img, (x, y), 2, [0, 0, 255], -1)
+        points.append((x, y))
 
-    if(event == cv2.EVENT_RBUTTONDOWN):
-        blue  = img[y, x, 0]
-        green = img[y, x, 1]
-        red   = img[y, x, 2] 
-        txt = 'B: ' + str(blue) + ' G: ' + str(green) + ' R: ' + str(red)
-        cv2.putText(img, txt, (x, y), font, 0.5, (200, 100, 100), 2, cv2.LINE_AA)
+        if(len(points) > 1) :
+            cv2.line(img, points[-1], points[-2], (0, 0, 255), 3)
+        
+        cv2.putText(img, txt, (x, y), font, 0.5, (0, 255, 255), 2, cv2.LINE_AA)
 
     cv2.imshow('image', img)
 
 img = cv2.imread("lena.jpg", -1)
 cv2.imshow('image', img)
+points = [] 
 cv2.setMouseCallback('image', ClickEvent)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
