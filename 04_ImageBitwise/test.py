ef63a2a3fbacef5d36a1164f0755109a418fc957
Mohamed Nagy <mohmmednagy@gmail.com>
2024-04-27 02:01:13 +0300

    Perform XOR Operation

--- 04_ImageBitwise/test.py
+++ 04_ImageBitwise/test.py
@@ -5,13 +5,16 @@ img1 = np.zeros((250, 500, 3), np.uint8)
 img1 = cv2.rectangle(img1, (0, 0), (250, 250), (255, 255, 255), -1)
 
 img2 = np.zeros((250, 500, 3), np.uint8)
-img2 = cv2.rectangle(img2, (200, 0), (300, 100), (255, 255, 255), -1)
+img2 = cv2.rectangle(img2, (200,75), (300, 175), (255, 255, 255), -1)
 
 cv2.imshow("image1", img1)
 cv2.imshow("image2", img2)
 
-bitAND = cv2.bitwise_and(img1, img2)
-cv2.imshow("bitAND", bitAND)
+# bitAND = cv2.bitwise_and(img1, img2)
+# cv2.imshow("bitAND", bitAND)
+
+bitXOR = cv2.bitwise_xor(img1, img2)
+cv2.imshow("bitXOR", bitXOR)
 
 k = cv2.waitKey(0)
 cv2.destroyAllWindows()
