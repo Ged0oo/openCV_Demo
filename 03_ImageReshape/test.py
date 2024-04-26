e21196cc0a4440a3a14c3d52dffdd7d0ed9153b2
Mohamed Nagy <mohmmednagy@gmail.com>
2024-04-27 01:38:08 +0300

    Add two photos

--- 03_ImageReshape/test.py
+++ 03_ImageReshape/test.py
@@ -1,13 +1,20 @@
 import cv2
+import numpy as np
 
-img = cv2.imread("test.jpg")
-cv2.imshow("image", img)
+def ClickEvent(event, x, y, flages, param):
+    if(event == cv2.EVENT_LBUTTONDOWN):
+        cv2.circle(img, (x, y), 2, [0, 0, 255], -1)
+    cv2.imshow('image', img)
 
-print(img.shape)
-print(img.size)
-print(img.dtype)
-b,g,r = cv2.split(img)
-img = cv2.merge((b,g,r))
- 
+img  = cv2.imread("test.jpg")
+img2 = cv2.imread("lena.jpg")
+final = cv2.add(img, img2)
+
+img  = cv2.resize(img ,  (512, 512))
+img2 = cv2.resize(img2,  (512, 512))
+
+cv2.imshow("image", final)
+cv2.setMouseCallback('image', ClickEvent)
+
 k = cv2.waitKey(0)
 cv2.destroyAllWindows()
